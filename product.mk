8677bcde1287e0b4d300121300ab59ef38000247
officialputuid <officialputuid@hack.id>
2023-10-30 11:11:34 +0800

    SystemUIClocks: Update from husky_beta-user 14 U1B2.230922.006 10879723 release-keys
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- product.mk
+++ product.mk
@@ -1,12 +1,11 @@
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/SystemUIClocks
-
-# Clocks
+# SystemUIClocks
+PRODUCT_SOONG_NAMESPACES += vendor/SystemUIClocks
 PRODUCT_PACKAGES += \
     SystemUIClocks-BigNum \
     SystemUIClocks-Calligraphy \
     SystemUIClocks-Flex \
     SystemUIClocks-Growth \
     SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
     SystemUIClocks-NumOverlap \
     SystemUIClocks-Weather
